da03befe8b2abedfa08928755ee381467f34dc69
ssalenik <stepan.salenikovich@gmail.com>
2012-03-19 00:31:28 -0400

    added needed structs

--- 427ass3/sillythreads/sillythreads.h
+++ 427ass3/sillythreads/sillythreads.h
@@ -15,11 +15,35 @@
  *
  * =====================================================================================
  */
+
+/* includes */
 #include <slack/std.h>
 #include <slack/list.h>
 #include <ucontext.h>
 
+/* data structures */
+typedef struct {
+	int index;							// index of semaphore used to address it
+	int init_value;						// initial value of semaphore
+	int value;							// current value of semaphore
+	List *thread_queue;					// queue of threads waiting on semaphore
+	list_release_t *queue_destructor;	// pointer to destroy queue
 
+}semaphore_t;
+
+typedef enum { RUNNING, RUNNABLE, WAITING, EXITING }state_t;
+
+typedef struct {
+	ucontext_t context;				// context
+	char* thread_name;				// thread name
+	int thread_id;					// thread id; same as its id in the thread array
+	state_t state;					// current state of thread
+	void (*threadfunc)();			// pointer to the function which will run in this thread
+	int stacksize;					// size of stack for this thread
+	int runtime;					// total time spent running on CPU
+}thread_control_block_t;
+
+/* function prototypes */
 int init_my_threads();
 int create_my_thread(char *threadname, void (*threadfunc)(), int stacksize);
 void exit_my_thread();
