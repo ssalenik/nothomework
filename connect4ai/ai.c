feefc6a209dd10a9f8b3903563aafa14a1b82524
Stepan Salenikovich <stepan.salenikovich@gmail.com>
2013-02-02 03:14:53 -0500

    adding ai and sources

---
+++ connect4ai/ai.c
@@ -0,0 +1,13 @@
+#import "ai.h"
+
+/* checks if end-game has been reached
+ * returns:
+ * 0 - if not endgame
+ * 1 - if white won
+ * 2 - if black won
+ */
+int check_endgame(char* game) {
+	int i, j;
+
+	return 0;
+}
---
+++ connect4ai/ai.h
@@ -0,0 +1,6 @@
+#ifndef AI_H_
+#define AI_H_
+
+int check_endgame(char*);
+
+#endif /* AI_H_ */
