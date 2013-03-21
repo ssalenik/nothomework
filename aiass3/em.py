6b296f972bcd81f05f50349ccacc05d5d2f13d06
Stepan Salenikovich <stepan.salenikovich@gmail.com>
2013-03-21 00:30:32 -0400

    can now display covariance ellipse

--- aiass3/em.py
+++ aiass3/em.py
@@ -2,6 +2,9 @@ import argparse
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+from matplotlib.patches import Ellipse
+
+from cov_ellipse import *
 
 defaultImageLocation = "images/image1_a.png"
 
@@ -11,10 +14,34 @@ imageLocation = parser.parse_args().image
 
 print "using image: %s" % imageLocation
 
+# read the image
 img = mpimg.imread(imageLocation)
+# img[Y][X][R G B]
+# print img = the entire contents of the array
+# print len(img[0][0]) = 20
+# img[0][19] = [0, 0, 1]  # makes the pixel blue
 
-# print img
+print img[15][15]
 
-imgplot = plt.imshow(img)
+# get the image resolution
+imgYLim = len(img)
+imgXLim = len(img[0])
 
-plt.show()
+imgplot = plt.imshow(img)	# create plot
+imgplot.set_interpolation('nearest') # we don't want interpolation so edges of pixels aren't "blurry"
+plt.plot(15,15, marker='o', color='b', markersize=15, label="initial mean")
+plt.plot(10,10, marker='o', color='g', markersize=10, label="final mean")
+plt.legend(loc=4)
+plt.ylim([imgYLim - 1,0])
+plt.xlim([0,imgXLim - 1])
+
+cov = np.array([[4., 0.], [0., 4.]])
+pos = np.array([7,7])
+
+plot_cov_ellipse(cov, pos)
+
+pos = np.array([15,15])
+
+plot_cov_ellipse(cov, pos)
+
+plt.show()	# show plot
